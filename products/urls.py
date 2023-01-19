8112a3423296734fcc07b7daeac4d03e42e17fd9
Aman Abdyraev <abdyraev.aman@gmail.com>
2023-01-19 16:15:16 +0600

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views import ProfessionalViewSet
 
 urlpatterns = [
     path('products/', ProfessionalViewSet.as_view({'get': 'list'})),
-    path('products/create', ProfessionalViewSet.as_view({'post': 'create'})),
+    path('products/create/', ProfessionalViewSet.as_view({'post': 'create'})),
     path('products/<int:pk>/', ProfessionalViewSet.as_view({'get': 'retrieve'})),
     path('products/update/<int:pk>/', ProfessionalViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
     path('products/delete/<int:pk>/', ProfessionalViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
